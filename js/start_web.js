fb0c12c70f32bb55d606eafe45fb915488775702
Mirexon™ <savrda50@gmail.com>
2026-04-07 13:22:11 +0200

    Deleted all files because crash all projects

--- js/start_web.js
+++
@@ -1,9 +0,0 @@
-console.log('Loader spustený');
-    setTimeout(() => {
-      console.log('Fade-out');
-      document.getElementById('intro-loader').classList.add('fade-out');
-      setTimeout(() => {
-        console.log('PRESMERUJEM na index.html');
-        window.location.href = 'index.html';
-      }, 1000);
-    }, 3500);
