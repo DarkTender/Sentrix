de5b47a2976111d65c303a6e76ec29d7edca8dfa
Mirexon™ <savrda50@gmail.com>
2026-04-07 13:46:21 +0200

    add header&footer

---
+++ views/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>SENTRIX LAB</title>
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+
+<div class="layout">
+
+  <div class="sidebar">
+    <h2>SENTRIX</h2>
+
+    <a href="challenges.php">Challenges</a>
+    <a href="#">Leaderboard</a>
+    <a href="#">Profile</a>
+    <a href="logout.php">Logout</a>
+  </div>
+
+  <div class="content">
