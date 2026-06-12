1115a4f81b9cfb90cde9b163f6520985b9888c9c
Dušan Šavrda <82323669+DarkTender@users.noreply.github.com>
2026-06-12 08:37:57 +0200

    Refactor challenge template with improved structure

--- public/admin/template_challenge.php
+++ public/admin/template_challenge.php
@@ -1,43 +1,77 @@
-<?php
+<?php require_once __DIR__ . '/../../views/header.php'; ?>
+<?php 
+if (isset($_GET['source'])) {
+    highlight_file(__FILE__);
+}
+if (isset($_GET['source'])) {
+    $file = "challenges/challenge" . $_GET['id'] . ".php";
 
-echo "<pre>";
-print_r($_FILES);
-echo "</pre>";
-
-if (
-    isset($_FILES['challenge_file']) &&
-    $_FILES['challenge_file']['error'] === UPLOAD_ERR_OK
-) {
-
-    $uploadDir = __DIR__ . '/uploads/';
-
-    if (!is_dir($uploadDir)) {
-        mkdir($uploadDir, 0777, true);
-    }
-
-    $fileName = basename($_FILES['challenge_file']['name']);
-
-    if (
-        move_uploaded_file(
-            $_FILES['challenge_file']['tmp_name'],
-            $uploadDir . $fileName
-        )
-    ) {
-        echo "<h2>UPLOAD OK</h2>";
-        echo "Saved to: " . $uploadDir . $fileName;
-    } else {
-        echo "<h2>UPLOAD FAILED</h2>";
+    if (file_exists($file)) {
+        highlight_file($file);
+        exit;
     }
 }
-
 ?>
+<main class="challenge-container">
 
-<form method="POST" enctype="multipart/form-data">
+  <div class="challenge-box">
 
-    <input type="file" name="challenge_file">
+    <h1 class="challenge-title">🕵️ Hidden Source Lab</h1>
 
-    <button type="submit">
-        TEST UPLOAD
-    </button>
+    <p class="challenge-desc">
+      Pozri source stránky a nájdi skryté heslo.
+    </p>
 
-</form>
+    <div class="terminal-input" style="margin-bottom:15px;">
+      <span class="prompt">system:</span>
+      <span>Welcome to secure panel...</span>
+    </div>
+
+    <!-- dev_note: try ?debug -->
+    <form method="POST" class="challenge-form">
+
+      <div class="terminal-input">
+        <span class="prompt">password:</span>
+        <input type="text" name="answer" placeholder="Enter password..." required>
+      </div>
+
+      <button type="submit" class="challenge-submit">LOGIN</button>
+
+    </form>
+
+    <?php
+    $result = null;
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        $userAnswer = trim($_POST['answer']);
+
+        if ($userAnswer === "WerZ23...12poREtzuF") {
+            $result = "correct";
+        } else {
+            $result = "wrong";
+        }
+    }
+    ?>
+
+    <?php if ($result === "correct"): ?>
+      <div class="result success">
+        ✅ ACCESS GRANTED  
+        <br><br>
+        FLAG{SOURCE_MASTER}
+      </div>
+    <?php elseif ($result === "wrong"): ?>
+      <div class="result error">
+        ❌ ACCESS DENIED"
+      </div>
+    <?php endif; ?>
+
+    <div style="margin-top:20px; color:#64748b;">
+    💡What's wrong with these words?? - ASSWORD IDDEN IN P................
+    </div>
+
+  </div>
+
+</main>
+
+<?php require_once __DIR__ . '/../../views/footer.php'; ?>
